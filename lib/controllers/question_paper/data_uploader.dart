146626ef8c3029f12c8e4b4e32092c68cb32d653
SupunJayaweera <jayaweera_st_e22@engug.ruh.ac.lk>
2023-10-27 18:55:27 +0530

    2nd commit

---
+++ lib/controllers/question_paper/data_uploader.dart
@@ -0,0 +1,52 @@
+
+
+import 'dart:convert';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
+import 'package:mobile_quiz_app/firebase_ref/references.dart';
+import 'package:mobile_quiz_app/models/question_paper_model.dart';
+
+class DataUploader extends GetxController{
+  @override
+  void onReady(){
+    uploadData();
+    super.onReady();
+  }
+
+  Future<void> uploadData()  async {
+
+    final fireStore = FirebaseFirestore.instance;
+
+    final manifestContent = await DefaultAssetBundle.of(Get.context!)
+    .loadString("AssetManifest.json");
+    final Map<String, dynamic> manifestMap = json.decode(manifestContent);
+    final papersInAssets = manifestMap.keys
+    .where((path) => 
+      path.startsWith("assets/DB/papers") && path.contains(".json"))
+    .toList();
+
+    List<QuestionPaperModel> questionPapers = [];
+
+    for(var paper in papersInAssets){
+      String stringPaperContent = await rootBundle.loadString(paper);
+      questionPapers.add(QuestionPaperModel.fromJson(json.decode(stringPaperContent)));
+    }
+    //print('Items number ${questionPapers[0].description}');
+    var batch = fireStore.batch();
+
+    for(var paper in questionPapers){
+      batch.set(questionPaperRF.doc(paper.id), {
+        "title": paper.title,
+        "image_url" : paper.imageUrl,
+        "description": paper.description,
+        "time_seconds" : paper.timeSeconds,
+        "questions_count" : paper.questions == null?0:paper.questions!.length,
+      });
+    }
+    await batch.commit();
+
+  }
+}
